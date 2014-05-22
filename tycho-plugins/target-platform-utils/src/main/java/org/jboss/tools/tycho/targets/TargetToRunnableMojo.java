6eb0048e63018ea5addd9f2c7effd8ff0f00dc39
Denis Golovin <dgolovin@gmail.com>
2014-05-21 17:58:16 -0700

    Mojo to buil runnable representation of target platform for use in eclipse: 1. can be used as installation type 2. has sources

--- tycho-plugins/target-platform-utils/src/main/java/org/jboss/tools/tycho/targets/TargetToRunnableMojo.java
+++ tycho-plugins/target-platform-utils/src/main/java/org/jboss/tools/tycho/targets/TargetToRunnableMojo.java
@@ -1,38 +1,61 @@
-/*******************************************************************************
- * Copyright (c) 2010, 2012 SAP AG and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+/**
+ * Copyright (c) 2012, Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *     SAP AG - initial API and implementation
- *******************************************************************************/
+ * Contributor:
+ *     Mickael Istria (Red Hat, Inc.) - initial API and implementation
+ *     Denis Golovin (Exadel, Inc) - target to runnable implementation
+ */
 package org.jboss.tools.tycho.targets;
 
 import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.repository.RepositorySystem;
+import org.codehaus.plexus.component.annotations.Requirement;
+import org.codehaus.plexus.logging.Logger;
 import org.eclipse.sisu.equinox.EquinoxServiceFactory;
 import org.eclipse.tycho.BuildOutputDirectory;
+import org.eclipse.tycho.artifacts.TargetPlatform;
 import org.eclipse.tycho.core.TargetPlatformConfiguration;
 import org.eclipse.tycho.core.facade.TargetEnvironment;
+import org.eclipse.tycho.core.resolver.shared.MavenRepositoryLocation;
 import org.eclipse.tycho.core.utils.TychoProjectUtils;
+import org.eclipse.tycho.osgi.adapters.MavenLoggerAdapter;
 import org.eclipse.tycho.p2.facade.RepositoryReferenceTool;
+import org.eclipse.tycho.p2.resolver.TargetDefinitionFile;
+import org.eclipse.tycho.p2.resolver.facade.P2ResolutionResult;
+import org.eclipse.tycho.p2.resolver.facade.P2Resolver;
+import org.eclipse.tycho.p2.resolver.facade.P2ResolverFactory;
+import org.eclipse.tycho.p2.resolver.facade.P2ResolutionResult.Entry;
+import org.eclipse.tycho.p2.target.facade.TargetDefinition;
+import org.eclipse.tycho.p2.target.facade.TargetPlatformConfigurationStub;
+import org.eclipse.tycho.p2.target.facade.TargetDefinition.InstallableUnitLocation;
+import org.eclipse.tycho.p2.target.facade.TargetDefinition.Location;
+import org.eclipse.tycho.p2.target.facade.TargetDefinition.Repository;
+import org.eclipse.tycho.p2.target.facade.TargetDefinition.Unit;
 import org.eclipse.tycho.p2.tools.RepositoryReferences;
 import org.eclipse.tycho.p2.tools.director.shared.DirectorCommandException;
 import org.eclipse.tycho.p2.tools.director.shared.DirectorRuntime;
-import org.eclipse.tycho.plugins.p2.director.Product;
-import org.eclipse.tycho.plugins.p2.director.ProductConfig;
-import org.eclipse.tycho.plugins.p2.director.runtime.StandaloneDirectorRuntimeFactory;
+import org.eclipse.tycho.p2.tools.mirroring.facade.IUDescription;
+import org.omg.CosNaming.NamingContextExtPackage.AddressHelper;
 
 /**
- * @phase package
- * @goal materialize-products
+ * @goal mirror-target-to-runnable
  */
 public final class TargetToRunnableMojo extends AbstractMojo {
 
@@ -55,145 +78,134 @@ public final class TargetToRunnableMojo extends AbstractMojo {
     MavenSession getSession() {
         return session;
     }
-	
-    public enum InstallationSource {
-        targetPlatform, repository
-    }
-
-    public enum DirectorRuntimeType {
-        internal, standalone
-    }
 
     /** @component */
     private EquinoxServiceFactory osgiServices;
 
     /** @component */
-    private RepositoryReferenceTool repositoryReferenceTool;
+    private Logger logger;
+
+    /** @parameter default-value="JavaSE-1.6" */
+    private String executionEnvironment;
 
     /**
-     * @parameter expression="${project.build.directory}/${project.artifactId}.runnable"
+     * @parameter expression="${project.build.directory}/${project.artifactId}.install"
      */
     private File outputRepository;
-
-    
-    /** @component */
-    private StandaloneDirectorRuntimeFactory standaloneDirectorFactory;
 
     /** @parameter default-value="DefaultProfile" */
     private String profile;
 
-    /** @parameter */
-    private List<ProfileName> profileNames;
-
     /** @parameter default-value="true" */
     private boolean installFeatures;
-
+    
     /**
-     * Installation source to be used for the director calls. Can be
-     * <ul>
-     * <li><code>targetPlatform</code> - to use the target platform as source (default)</li>
-     * <li><code>repository</code> - to use the p2 repository in <tt>target/repository/</tt> as
-     * source. This ensures that it is possible to install the product from that repository using an
-     * (external) director application.
-     * </ul>
-     * 
-     * @parameter default-value="targetPlatform"
+     * @parameter expression="${project.basedir}/${project.artifactId}.target"
      */
-    private InstallationSource source;
-
-    /**
-     * Runtime context in which the director application is executed. Can be
-     * <ul>
-     * <li><code>internal</code> - to use the director application from Tycho's embedded OSGi
-     * runtime (default)</li>
-     * <li><code>standalone</code> - to create and use a stand-alone installation of the director
-     * application. This option is needed if the product to be installed includes artifacts with
-     * meta-requirements (e.g. to a non-standard touchpoint action). Requires that the
-     * <code>source</code> parameter is set to <code>repository</code>.
-     * </ul>
-     * 
-     * @parameter default-value="internal"
-     */
-    private DirectorRuntimeType directorRuntime;
+    private File sourceTargetFile;
 
     // TODO extract methods
     public void execute() throws MojoExecutionException, MojoFailureException {
-    	List<String> ius = null;
+
         DirectorRuntime director = getDirectorRuntime();
-        RepositoryReferences sources = getSourceRepositories();
-        for (String iu : ius) {
-            for (TargetEnvironment env : getEnvironments()) {
-                DirectorRuntime.Command command = director.newInstallCommand();
-
-                File destination = getProductMaterializeDirectory(product, env);
-                String rootFolder = product.getRootFolder(env.getOs());
-                if (rootFolder != null && rootFolder.length() > 0) {
-                    destination = new File(destination, rootFolder);
-                }
-
-                command.addMetadataSources(sources.getMetadataRepositories());
-                command.addArtifactSources(sources.getArtifactRepositories());
-                command.addUnitToInstall(product.getId());
-                command.setDestination(destination);
-                command.setProfileName(ProfileName.getNameForEnvironment(env, profileNames, profile));
-                command.setEnvironment(env);
-                command.setInstallFeatures(installFeatures);
-                getLog().info(
-                        "Installing product " + product.getId() + " for environment " + env + " to "
-                                + destination.getAbsolutePath());
-
-                try {
-                    command.execute();
-                } catch (DirectorCommandException e) {
-                    throw new MojoFailureException("Installation of product " + product.getId() + " for environment "
-                            + env + " failed", e);
-                }
+     
+        try {
+        	TargetDefinitionFile target = TargetDefinitionFile.read(sourceTargetFile);
+    		DirectorRuntime.Command command = director.newInstallCommand();
+    		List<URI> repos = new ArrayList<URI>();
+        	for (final Location loc : target.getLocations()) {
+        		List<? extends TargetDefinition.Repository> targetRepos = ((InstallableUnitLocation)loc).getRepositories();
+        		for (Repository repository : targetRepos) {
+        			repos.add(repository.getLocation());
+				}
+        	}
+    		command.addMetadataSources(repos);
+    		command.addArtifactSources(repos);
+    		
+    		TargetPlatformConfigurationStub tpConfiguration = new TargetPlatformConfigurationStub();
+            List<TargetEnvironment> environments = Collections.singletonList(TargetEnvironment.getRunningEnvironment());
+            tpConfiguration.setEnvironments(environments);
+            tpConfiguration.addTargetDefinition(target);
+            
+            P2ResolverFactory factory = this.osgiServices.getService(P2ResolverFactory.class);
+            P2Resolver tpResolver = factory.createResolver(new MavenLoggerAdapter(this.logger, getLog().isDebugEnabled()));
+            tpResolver.setEnvironments(environments);
+            
+            for (Location loc : target.getLocations()) {
+            	if (loc instanceof InstallableUnitLocation) {
+            		InstallableUnitLocation p2Loc = (InstallableUnitLocation) loc;
+            		for (Unit unit : p2Loc.getUnits()) {
+            			// resolve everything in TP
+            			tpResolver.addDependency(P2Resolver.TYPE_INSTALLABLE_UNIT, unit.getId(), "[" + unit.getVersion() + "," + unit.getVersion() + "]");
+            		}
+            	}
             }
+            P2ResolutionResult result = tpResolver.resolveMetadata(tpConfiguration, executionEnvironment);
+
+            Set<String> sourcesFound = new HashSet<String>();
+            Set<String> regularArtifacts = new HashSet<String>();
+        	for (Entry entry : result.getArtifacts()) {
+        		if (entry.getId().endsWith(".source")) {
+        			sourcesFound.add(entry.getId().substring(0, entry.getId().length() - ".source".length()));
+        		} else if (entry.getId().endsWith(".source.feature.group")) {
+        			sourcesFound.add(entry.getId().replace(".source.feature.group", ".feature.group"));
+        		} else {
+        			regularArtifacts.add(entry.getId());
+        		}
+        	}
+        	
+        	Set<String> artifactsWithoutSources = new HashSet<String>(regularArtifacts);
+        	artifactsWithoutSources.removeAll(sourcesFound);
+        	
+        	if (!artifactsWithoutSources.isEmpty()) {
+        		TargetPlatformConfigurationStub sites = new TargetPlatformConfigurationStub();
+        		
+        		for (Location loc : target.getLocations()) {
+        			if (loc instanceof InstallableUnitLocation) {
+        				InstallableUnitLocation location = (InstallableUnitLocation)loc;
+        				for (Repository repo : location.getRepositories()) {
+        					sites.addP2Repository(new MavenRepositoryLocation(repo.getId(), repo.getLocation()));
+        				}
+        			}
+        		}
+        		TargetPlatform sitesTP = factory.getTargetPlatformFactory().createTargetPlatform(sites, new MockExecutionEnvironment(), null, null);
+        		for (String unitId : artifactsWithoutSources) {
+        			String sourceUnitId = unitId + ".source";
+        			// ignore .feature.source
+        			P2ResolutionResult resolvedSource = tpResolver.resolveInstallableUnit(sitesTP, sourceUnitId, null);
+        			if (resolvedSource.getArtifacts().size() > 0 || resolvedSource.getNonReactorUnits().size() > 0) {
+        				command.addUnitToInstall(sourceUnitId + "/" + resolvedSource.getArtifacts().iterator().next().getVersion());
+        			}
+        		}
+        	}
+
+        	for (final Location loc : target.getLocations()) {
+            	if (loc instanceof InstallableUnitLocation) {
+            		for (Unit unit : ((InstallableUnitLocation)loc).getUnits()) {
+            			command.addUnitToInstall(unit.getId()+"/"+ unit.getVersion());
+            		}
+            	}
+        	}
+        	
+		
+    		command.setDestination(outputRepository);
+    		command.setProfileName(profile);
+    		command.setEnvironment(TargetEnvironment.getRunningEnvironment());
+    		command.setInstallFeatures(installFeatures);
+    		getLog().info(
+    				"Converting target to runnable " 
+    						+ outputRepository.getAbsolutePath());
+
+            command.execute();
+
+        } catch (DirectorCommandException e) {
+            throw new MojoFailureException("Converting target to runnable failed", e);
         }
+
     }
 
     private DirectorRuntime getDirectorRuntime() throws MojoFailureException, MojoExecutionException {
-        switch (directorRuntime) {
-        case internal:
-            // director from Tycho's OSGi runtime
-            return osgiServices.getService(DirectorRuntime.class);
-
-        case standalone:
-            // separate director installation in the target folder
-            return standaloneDirectorFactory.createStandaloneDirector(getBuildDirectory().getChild("director"),
-                    getSession().getLocalRepository());
-
-        default:
-            throw new MojoFailureException("Unsupported value for attribute 'directorRuntime': \"" + directorRuntime
-                    + "\"");
-        }
-    }
-
-    private RepositoryReferences getSourceRepositories() throws MojoExecutionException, MojoFailureException {
-        switch (source) {
-        case targetPlatform:
-            return getTargetPlatformRepositories();
-
-        case repository:
-            return getBuildOutputRepository();
-
-        default:
-            throw new MojoFailureException("Unsupported value for attribute 'source': \"" + source + "\"");
-        }
-    }
-
-    private RepositoryReferences getBuildOutputRepository() {
-        // TODO share "repository" constant?
-        File buildOutputRepository = getBuildDirectory().getChild("repository");
-
-        RepositoryReferences result = new RepositoryReferences();
-        result.addMetadataRepository(buildOutputRepository);
-        result.addArtifactRepository(buildOutputRepository);
-        return result;
-    }
-
-    private RepositoryReferences getTargetPlatformRepositories() throws MojoExecutionException, MojoFailureException {
-        int flags = RepositoryReferenceTool.REPOSITORIES_INCLUDE_CURRENT_MODULE;
-        return repositoryReferenceTool.getVisibleRepositories(getProject(), getSession(), flags);
+        // director from Tycho's OSGi runtime
+        return osgiServices.getService(DirectorRuntime.class);
     }
 }
